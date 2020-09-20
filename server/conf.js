7e4fdb0bf8b1c8f0a7de624dc35870057b127652
Papabyte <bcreze@protonmail.com>
2020-09-20 10:27:19 +0700

    mainnet oracle

--- server/conf.js
+++ server/conf.js
@@ -8,5 +8,5 @@ exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 
 exports.issuer_base_aa = "K6KIVWTU5C4JPSIQF5F42XYIQIIBHOIP";
 
-exports.oracle_address = process.env.testnet ? '4EHFJW5EY74DG6PGSXX5CHF5F2FFIJX4' : '';
-exports.oracle_pairing_code = process.env.testnet ? 'A1ZoOTmbiKZ1IaUzRn+z9OBW8xQnqDS3EawWaFE6lyY0@obyte.org/bb-test#0000' : ''; 
+exports.oracle_address = process.env.testnet ? '4EHFJW5EY74DG6PGSXX5CHF5F2FFIJX4' : 'TKT4UESIKTTRALRRLWS4SENSTJX6ODCW';
+exports.oracle_pairing_code = process.env.testnet ? 'A1ZoOTmbiKZ1IaUzRn+z9OBW8xQnqDS3EawWaFE6lyY0@obyte.org/bb-test#0000' : 'Ar1O7dGgkkcABYNAbShlY2Pbx6LmUzoyRh6F14vM0vTZ@obyte.org/bb#0000'; 
