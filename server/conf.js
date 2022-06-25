b2c2f5d60b75fe5a15299908e3dd68a9178a20e1
tony <tonyofbyteball@users.noreply.github.com>
2022-06-25 17:52:27 +0200

    newer deps

--- server/conf.js
+++ server/conf.js
@@ -10,3 +10,6 @@ exports.issuer_base_aa = "K6KIVWTU5C4JPSIQF5F42XYIQIIBHOIP";
 
 exports.oracle_address = process.env.testnet ? '4EHFJW5EY74DG6PGSXX5CHF5F2FFIJX4' : 'TKT4UESIKTTRALRRLWS4SENSTJX6ODCW';
 exports.oracle_pairing_code = process.env.testnet ? 'A1ZoOTmbiKZ1IaUzRn+z9OBW8xQnqDS3EawWaFE6lyY0@obyte.org/bb-test#0000' : 'Ar1O7dGgkkcABYNAbShlY2Pbx6LmUzoyRh6F14vM0vTZ@obyte.org/bb#0000'; 
+
+exports.bNoPassphrase = true;
+exports.refreshHistoryOnlyAboveMci = 8950000;
